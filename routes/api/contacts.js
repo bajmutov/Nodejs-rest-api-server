5ff7c19783d1c5235e8057d292e6c3f4ac2440e7
unknown <bajmutov@gmail.com>
2023-11-23 17:26:59 +0200

    init

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -14,11 +14,11 @@ router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-router.delete('/:contactId', async (req, res, next) => {
+router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-router.put('/:contactId', async (req, res, next) => {
+router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
